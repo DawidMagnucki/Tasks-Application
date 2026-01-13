49122d4dfc588a9c436182e28518d18bd9dd674a
Dawid Magnucki <david.magnucki@gmail.com>
2026-01-13 18:31:43 +0100

    22.4 + Zadanie: problem do rozwiązania

--- src/main/java/com/crud/tasks/service/TrelloService.java
+++ src/main/java/com/crud/tasks/service/TrelloService.java
@@ -18,16 +18,16 @@ import static java.util.Optional.ofNullable;
 public class TrelloService {
 
     public static final String SUBJECT = "Tasks: New Trello card";
-    private final TrelloClient trelloService;
+    private final TrelloClient trelloClient;
     private final SimpleEmailService emailService;
     private final AdminConfig adminConfig;
 
     public List<TrelloBoardDto> fetchTrelloBoards() {
-        return trelloService.getTrelloBoards();
+        return trelloClient.getTrelloBoards();
     }
 
     public CreatedTrelloCard createTrelloCard(final TrelloCardDto trelloCardDto) {
-        CreatedTrelloCard newCard = trelloService.createNewCard(trelloCardDto);
+        CreatedTrelloCard newCard = trelloClient.createNewCard(trelloCardDto);
         ofNullable(newCard).ifPresent(card -> emailService.send(
                 Mail.builder()
                         .mailTo(adminConfig.getAdminMail())
